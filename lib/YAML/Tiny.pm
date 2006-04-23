768ea8535040a398a8829e9184c6492d0ffa3cc3
adam <adam@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2006-04-23 22:50:56 +0000

    Moving preamble

--- lib/YAML/Tiny.pm
+++ lib/YAML/Tiny.pm
@@ -237,46 +237,6 @@ __END__
 
 YAML::Tiny - Read/Write YAML files with as little code as possible
 
-=head1 SYNOPSIS
-
-    #############################################
-    # In your file
-    
-    ---
-    rootproperty: blah
-    section:
-      one: two
-      three: four
-      Foo: Bar
-      empty: ~
-    
-    #############################################
-    # In your program
-    
-    use YAML::Tiny;
-    
-    # Create a YAML file
-    my $yaml = YAML::Tiny->new;
-    
-    # Open the config
-    $yaml = YAML::Tiny->read( 'file.yml' );
-    
-    # Reading properties
-    my $root = $yaml->[0]->{rootproperty};
-    my $one  = $yaml->[0]->{section}->{one};
-    my $Foo  = $yaml->[0]->{section}->{Foo};
-    
-    # Changing data
-    $yaml->[0]->{newsection} = { this => 'that' }; # Add a section
-    $yaml->[0]->{section}->{Foo} = 'Not Bar!';     # Change a value
-    delete $yaml->[0]->{section};                  # Delete a value or section
-    
-    # Add an entire document
-    $yaml->[1] = [ 'foo', 'bar', 'baz' ];
-    
-    # Save the file
-    $yaml->write( 'file.conf' );
-
 =head1 PREAMBLE
 
 B<WARNING: THIS MODULES IS HIGHLY EXPERIMENTAL AND SUBJECT TO CHANGE
@@ -321,6 +281,46 @@ methods, and I've implemented enough code to handle the following.
 
 And that's it. So do B<not> use this module for anything other than
 experimentation. It's only just getting started.
+
+=head1 SYNOPSIS
+
+    #############################################
+    # In your file
+    
+    ---
+    rootproperty: blah
+    section:
+      one: two
+      three: four
+      Foo: Bar
+      empty: ~
+    
+    #############################################
+    # In your program
+    
+    use YAML::Tiny;
+    
+    # Create a YAML file
+    my $yaml = YAML::Tiny->new;
+    
+    # Open the config
+    $yaml = YAML::Tiny->read( 'file.yml' );
+    
+    # Reading properties
+    my $root = $yaml->[0]->{rootproperty};
+    my $one  = $yaml->[0]->{section}->{one};
+    my $Foo  = $yaml->[0]->{section}->{Foo};
+    
+    # Changing data
+    $yaml->[0]->{newsection} = { this => 'that' }; # Add a section
+    $yaml->[0]->{section}->{Foo} = 'Not Bar!';     # Change a value
+    delete $yaml->[0]->{section};                  # Delete a value or section
+    
+    # Add an entire document
+    $yaml->[1] = [ 'foo', 'bar', 'baz' ];
+    
+    # Save the file
+    $yaml->write( 'file.conf' );
 
 =head1 DESCRIPTION
 
