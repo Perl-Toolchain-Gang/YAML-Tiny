3d9756cc2899c69dd2ee905497526c80fb9e8d2c
adamk@cpan.org <adamk@cpan.org@88f4d9cd-8a04-0410-9d60-8f63309c3137>
2008-05-14 07:31:56 +0000

    Small tweaks to YAML::Tiny

---
+++ t/19_errors.t
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+# Testing documents that should fail
+
+use strict;
+BEGIN {
+	$|  = 1;
+	$^W = 1;
+}
+
+use File::Spec::Functions ':ALL';
+use t::lib::Test;
+use Test::More tests => 1;
+use YAML::Tiny ();
+
+
+
+
+
+#####################################################################
+# Missing Features
+
+# We don't support raw nodes
+yaml_error( <<'END_YAML', 'Unsupported YAML feature' );
+---
+version: !!perl/hash:version 
+  original: v2.0.2
+  qv: 1
+  version: 
+    - 2
+    - 0
+    - 2
+END_YAML
+
--- t/lib/Test.pm
+++ t/lib/Test.pm
@@ -18,22 +18,34 @@ eval {
 	# So if it ever turns up, use it.
 	$YAML::UseVersion = 1;
 };
-my $HAVE_YAMLPM = !! $YAML::VERSION;
-sub have_yamlpm {
-	$HAVE_YAMLPM;
-}
+my $HAVE_YAMLPM = !! (
+	$YAML::VERSION
+	and
+	$YAML::VERSION >= 0.66
+);
+sub have_yamlpm { $HAVE_YAMLPM }
 
 # Do we have YAML::Syck to test against?
 eval {
 	require YAML::Syck;
 };
-my $HAVE_SYCK = !! $YAML::Syck::VERSION;
+my $HAVE_SYCK = !! (
+	$YAML::Syck::VERSION
+	and
+	$YAML::Syck::VERSION >= 1.04
+);
+sub have_syck { $HAVE_SYCK }
 
 # Do we have YAML::XS to test against?
 eval {
 	require YAML::XS;
 };
-my $HAVE_XS = !! $YAML::XS::VERSION;
+my $HAVE_XS = !! (
+	$YAML::XS::VERSION
+	and
+	$YAML::XS::VERSION >= 0.26
+);
+sub have_xs{ $HAVE_XS }
 
 # 22 tests per call to yaml_ok
 # 4  tests per call to load_ok
