98015165b300078338875a8447d66ddb9d7649a7
Karen Etheridge <ether@cpan.org>
2013-08-20 21:48:03 -0700

    bump version to 1.52

--- lib/YAML/Tiny.pm
+++ lib/YAML/Tiny.pm
@@ -15,7 +15,7 @@ BEGIN {
 	require 5.004;
 	require Exporter;
 	require Carp;
-	$YAML::Tiny::VERSION   = '1.51';
+	$YAML::Tiny::VERSION   = '1.52';
 	# $YAML::Tiny::VERSION   = eval $YAML::Tiny::VERSION;
 	@YAML::Tiny::ISA       = qw{ Exporter  };
 	@YAML::Tiny::EXPORT    = qw{ Load Dump };
