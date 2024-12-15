6772fa9a247ed045cb16b56b85540b0bcb07ceee
Karen Etheridge <ether@cpan.org>
2024-12-15 13:25:14 -0800

    increment $VERSION after 1.75 release

--- lib/YAML/Tiny.pm
+++ lib/YAML/Tiny.pm
@@ -6,7 +6,7 @@ package YAML::Tiny;
 # XXX-XDG Lancaster consensus was that it was sufficient until
 # proven otherwise
 
-our $VERSION = '1.75';
+our $VERSION = '1.76';
 
 #####################################################################
 # The YAML::Tiny API.
